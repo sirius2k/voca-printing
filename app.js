73ecabd5fd79ef98d2f58e5d5a611b68db65a011
박경욱 <kwpark@kwpark-MacBook-pro.local>
2020-09-30 21:54:34 +0900

    Added express middleware settings

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const path  = require('path')
+const history = require('connect-history-api-fallback')
 
 const app = express()
 const port = 3000
 
 app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(history())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => {
+/*
+For reference : 
+
+app.use('/api/metric', metricRouter);
+*/
+
+app.get('/hello', (req, res) => {
     res.send('Hello World!')
 })
 
